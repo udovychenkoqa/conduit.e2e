b97810a3ffef8e6e6e8bfb504f862e447db45e82
ant0d0v <gosu1203@gmail.com>
2024-12-28 22:28:54 +0200

    refractor

---
+++ tests/7-hw.spec.ts
@@ -0,0 +1,53 @@
+import { test, expect } from "@playwright/test";
+
+function isPositive(number) {
+  if (typeof number === "number") {
+    if (number > 0) {
+      console.log("number is positive");
+      return true;
+    } else if (number === 0) {
+      console.log("number is negative");
+      return false;
+    } else {
+      console.log("number is negative");
+      return false;
+    }
+  } else {
+    throw Error("pls use number to check if it positive");
+  }
+}
+test('positive numbers', async () => {
+  expect(isPositive(100)).toBeTruthy();
+  expect(isPositive(1.2)).toBeTruthy();
+  expect(isPositive(+2)).toBeTruthy();
+});
+
+test("negative numbers", async () => {
+  expect(isPositive(-1321321)).toBeFalsy();
+  expect(isPositive(-1.321321)).toBeFalsy();
+  expect(isPositive(-1 + -1)).toBeFalsy();
+});
+
+test("zero", async () => {
+  expect(isPositive(0)).toBeFalsy();
+});
+
+// Граничні значення
+test("boundary values", async () => {
+  expect(isPositive(1)).toBeTruthy(); 
+  expect(isPositive(0)).toBeFalsy(); 
+  expect(isPositive(-1)).toBeFalsy(); 
+});
+
+test("infinity values", async () => {
+  expect(isPositive(Infinity)).toBeTruthy(); 
+  expect(isPositive(-Infinity)).toBeFalsy();
+});
+
+// Помилки
+test("non-number", async () => {
+    expect(() => isPositive("2")).toThrowError("pls use number to check if it positive");
+    expect(() => isPositive(null)).toThrowError("pls use number to check if it positive");
+    expect(() => isPositive(undefined)).toThrowError("pls use number to check if it positive");
+    expect(() => isPositive({})).toThrowError("pls use number to check if it positive");
+});
